23438d80fec4961e850b8795ee4c2643058454aa
SAHOWI <wienecke.ch@gmail.com>
2020-01-19 19:38:15 +0100

    debugging on some weired lambda issue

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,7 @@ int main() {
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
 
-  // BEGIN
-  // start with middle lane 
-  int lane = 1;
-  // double ref_vel = 49.5; // MHP 
-  double ref_vel = 0;
-
-  if (lane == 1) { 
-    //
-    bool SUCCESS = true;
-  }
-
+  
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
@@ -69,10 +59,21 @@ int main() {
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-               uWS::OpCode opCode)) {
+               uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+// BEGIN
+  // start with middle lane 
+  int lane = 1;
+  // double ref_vel = 49.5; // MHP 
+  double ref_vel = 0;
+
+  if (lane == 1) { 
+    //
+    bool SUCCESS = true;
+  }
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
